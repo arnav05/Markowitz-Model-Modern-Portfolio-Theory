1ca568cf2094219ea167b0df2aa91ab3358ed1ca
Arnav Singh <a426sing@uwaterloo.ca>
2022-02-28 00:13:26 -0500

    Working code, need to be cleaned. Comments will be added

--- MarkowitzModel.py
+++ MarkowitzModel.py
@@ -1,12 +1,12 @@
 import numpy as np
-import yfinance as yf
-import pandas as pd
 import matplotlib.pyplot as plt
 import scipy.optimize as optimization
 
 
-# Average number of trading days
+# Average number of trading days in a year
 NUM_TRADING_DAYS = 252
+# For generating random portfolio weights
+NUM_PORTFOLIOS = 10000
 
 
 def show_data(data):
@@ -20,7 +20,84 @@ def calculate_return(data):
     return log_return[1:]
 
 
-# calculating annual metrics
+# calculating annual return
 def show_statistics(returns):
+    # Calculates average mean for a year
     print(returns.mean()*NUM_TRADING_DAYS)
+    # Calculates covariance between all stocks
     print(returns.cov()*NUM_TRADING_DAYS)
+
+
+def show_mean_variance(returns, weights):
+    # Calculating annual return
+    portfolio_return = np.sum(returns.mean()*weights) * NUM_TRADING_DAYS
+    portfolio_volatility = np.sqrt(np.dot(weights.T, np.dot(returns.cov()*NUM_TRADING_DAYS, weights)))
+    print(portfolio_return)
+    print(portfolio_volatility)
+
+
+def generate_portfolios(portfolio, log_returns):
+
+        portfolio_weights = []
+        portfolio_returns = []
+        portfolio_volatility = []
+
+        for _ in range(NUM_PORTFOLIOS):
+            w = np.random.random(len(portfolio))
+            # Normalizing weight to have sum of weights equal to 1
+            w /= np.sum(w)
+            portfolio_weights.append(w)
+            portfolio_returns.append(np.sum(log_returns.mean()*w)*NUM_TRADING_DAYS)
+            portfolio_volatility.append(np.sqrt(np.dot(w.T, np.dot(log_returns.cov()*NUM_TRADING_DAYS, w))))
+
+        return np.array(portfolio_weights), np.array(portfolio_returns), np.array(portfolio_volatility)
+
+
+def show_portfolios(returns, volatility):
+    plt.figure(figsize=(10,6))
+    plt.scatter(volatility, returns, c=returns/volatility, marker="o")
+    plt.xlabel('Expected Volatility')
+    plt.ylabel('Expected Returns')
+    plt.colorbar(label='Sharpe Ratio')
+    plt.show()
+
+
+def statistics(weights, returns):
+    portfolio_return = np.sum(returns.mean()*weights)*NUM_TRADING_DAYS
+    portfolio_volatility = np.sqrt(np.dot(weights.T, np.dot(returns.cov() * NUM_TRADING_DAYS, weights)))
+
+    return np.array([portfolio_return, portfolio_volatility, portfolio_return/portfolio_volatility])
+
+
+# scipy optimize module can find the minimum of a given function
+# maximum of a f(x) is the mimimum of -f(x)
+def min_function_sharpe(weights, returns):
+    return -statistics(weights, returns)[2]
+
+
+# The constraint is that sum of edge weights = 1
+# f(x) = 0 this is the function to minimize
+def optimize_portfolio(portfolio, weights, log_returns):
+    constraints = {'type': 'eq', 'fun': lambda x: np.sum(x) - 1}
+    # The weights can be 1 at most: 1 when 100% of money is invested into a single stock
+    bounds = tuple((0,1) for _ in range(len(portfolio)))
+    return optimization.minimize(fun=min_function_sharpe, x0=weights[0], args=log_returns, method='SLSQP',
+                          bounds=bounds, constraints=constraints)
+
+
+def print_optimal_portfolio(optimum, returns):
+    print("Optimal portfolio: ", optimum['x'].round(5))
+    print("Expected return, volatility and Sharpe ratio: ",
+          statistics(optimum['x'].round(3), returns))
+
+
+def show_optimal_portfolio(optimum, returns, portfolio_returns, portfolio_volatility):
+    plt.figure(figsize=(10, 6))
+    plt.scatter(portfolio_volatility, portfolio_returns, c=portfolio_returns / portfolio_volatility, marker="o")
+    plt.grid(True)
+    plt.xlabel('Expected Volatility')
+    plt.ylabel('Expected Returns')
+    plt.colorbar(label='Sharpe Ratio')
+    plt.plot(statistics(optimum['x'], returns)[1], statistics(optimum['x'], returns)[0], 'g*', markersize=20.0)
+    plt.show()
+
--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 from MarkowitzModel import *
+import yfinance as yf
+import pandas as pd
 
-start_date = '2017-01-01'
+# Historical data - define start and end date
+start_date = '2020-01-01'
 end_date = '2022-01-01'
 
 if __name__ == "__main__":
 
     # Requesting user to input their portfolio
-    print("\nPlease input ticker symbols of stocks/Bonds in your portfolio.\nAccepted example input: AAPL NVDA TSLA GE AMZN\n")
+    print("\nPlease input ticker symbols of stocks/Bonds in your portfolio.\nAccepted example input: AAPL NVDA Tsla ge AMZN\n")
     inputPortfolio = input("Portfolio: ")
 
     # Splitting input by spaces in between
     portfolio = inputPortfolio.split(' ')
 
+    # Dictionary for storing stock information with their ticker
     stock_data = {}
 
     print("\nFetching information and performing calculations\n")
-    # Checking if there is a mistake in the input
+    # Checking if there is a mistake in the input, if not downloading data
     for security in portfolio:
         ticker = yf.Ticker(security)
 
@@ -25,9 +29,12 @@ if __name__ == "__main__":
         stock_data[security] = ticker.history(start=start_date, end=end_date)['Close']
 
     dataset = pd.DataFrame(stock_data)
+    log_daily_returns = calculate_return(dataset)
 
-    show_data(dataset)
-    # dataset = download_data()
-    # show_data(dataset)
-    # log_daily_returns = calculate_return(dataset)
-    # show_statistics(log_daily_returns)
+    p_weights, p_returns, p_volatility = generate_portfolios(portfolio, log_daily_returns)
+    show_portfolios(p_returns, p_volatility)
+
+    optimum = optimize_portfolio(portfolio, p_weights, log_daily_returns)
+
+    print_optimal_portfolio(optimum, log_daily_returns)
+    show_optimal_portfolio(optimum, log_daily_returns, means, volatility)
